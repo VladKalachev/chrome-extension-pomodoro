428fe11141de62178473434dab009ddac68330bb
Vlad <vlvete1990@yandex.ru>
2022-09-09 13:11:15 +0300

    chore: popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,41 @@
 let tasks = [];
 
+function updateTime() {
+    chrome.storage.local.get(["timer", "timeOption"], (res) => {
+        const time = document.getElementById("time")
+        const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2, "0")
+        let seconds = "00"
+        if (res.timer % 60 != 0) {
+            seconds = `${60 - res.timer % 60}`.padStart(2, "0")
+        }
+        time.textContent = `${minutes}:${seconds}`
+    })
+}
+
+updateTime()
+setInterval(updateTime, 1000)
+
+const startTimerBtn = document.getElementById("start-timer-btn");
+startTimerBtn.addEventListener("click", () => {
+    chrome.storage.local.get(["isRunning"], (res) => {
+        chrome.storage.local.set({
+            isRunning: !res.isRunning,
+        }, () => {
+            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
+        })
+    })
+});
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+    chrome.storage.local.set({
+        timer: 0,
+        isRunning: false,
+    }, () => {
+        startTimerBtn.textContent = "Start Timer"
+    })
+});
+
 const addTaskBtn = document.getElementById("add-task-btn");
 addTaskBtn.addEventListener("click", () => addTask());
 
